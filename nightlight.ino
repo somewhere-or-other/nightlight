fa229a2b0c0b7d3f3a5a683df4fb3dacadf9b202
Lloyd Brown <lloyd_brown@byu.edu>
2017-08-17 11:44:33 -0600

    basic sketch for colors

--- nightlight.ino
+++ nightlight.ino
@@ -1,19 +1,51 @@
-
-int R1input = A0;
-int R2input = A1;
-
-// the setup routine runs once when you press reset:
+// color swirl! connect an RGB LED to the PWM pins as indicated
+// in the #defines
+// public domain, enjoy!
+ 
+#define REDPIN 5
+#define GREENPIN 6
+#define BLUEPIN 3
+ 
+#define FADESPEED 5     // make this higher to slow down
+ 
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+  pinMode(REDPIN, OUTPUT);
+  pinMode(GREENPIN, OUTPUT);
+  pinMode(BLUEPIN, OUTPUT);
 }
-
-// the loop routine runs over and over again forever:
+ 
+ 
 void loop() {
-  int sensorValue = analogRead(R1input);
-  Serial.println(sensorValue);
-  sensorValue = analogRead(R2input);
-  Serial.println(sensorValue);
-  
-  delay(250);        // delay in between reads for stability
+  int r, g, b;
+ 
+  // fade from blue to violet
+  for (r = 0; r < 256; r++) { 
+    analogWrite(REDPIN, r);
+    delay(FADESPEED);
+  } 
+  // fade from violet to red
+  for (b = 255; b > 0; b--) { 
+    analogWrite(BLUEPIN, b);
+    delay(FADESPEED);
+  } 
+  // fade from red to yellow
+  for (g = 0; g < 256; g++) { 
+    analogWrite(GREENPIN, g);
+    delay(FADESPEED);
+  } 
+  // fade from yellow to green
+  for (r = 255; r > 0; r--) { 
+    analogWrite(REDPIN, r);
+    delay(FADESPEED);
+  } 
+  // fade from green to teal
+  for (b = 0; b < 256; b++) { 
+    analogWrite(BLUEPIN, b);
+    delay(FADESPEED);
+  } 
+  // fade from teal to blue
+  for (g = 255; g > 0; g--) { 
+    analogWrite(GREENPIN, g);
+    delay(FADESPEED);
+  } 
 }
